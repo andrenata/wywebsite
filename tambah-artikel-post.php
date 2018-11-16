bcf8872057ed6311a11f4296f91a77d2a74e9427
andrenata <andrenata25@gmail.com>
2018-11-16 20:53:34 +0800

    Add Comment

--- tambah-artikel-post.php
+++ tambah-artikel-post.php
@@ -1,7 +1,9 @@
 <?php
 
+    // PANGGIL KONEKSI KE DATABASE
     include('koneksi.php');
 
+    // MENGAMBIL DATA YANG DI-INPUT
     $judul = $_POST["judul"];
     $artikel = $_POST["artikel"];
 
@@ -9,6 +11,7 @@
     $sql = "INSERT INTO posts(title_post, article) VALUES('$judul', '$artikel')";
     $query = mysqli_query($connect, $sql);
 
+    // REDIRECT KE HALAMAN BERANDA SETELAH INPUT DATA
     header("location:index.php");
 
 
