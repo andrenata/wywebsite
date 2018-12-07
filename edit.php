80fd3e7cefd639b4f5f27d1e9b4fcca2952d4c64
andrenata <andrenata25@gmail.com>
2018-12-07 20:08:07 +0800

    tambah edit.php

---
+++ edit.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Wisata Bali</title>
+    <!-- menghubungkan index dengan css -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="header">
+            <h1>My Website</h1>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <!-- PANGGIL MENU DARI FILE menu.php -->
+                <?php
+                    include('menu.php');
+                ?>
+            </ul>
+            <div class="clear"></div>
+        </div>
+
+        <div class="content">
+
+            <h2>Edit Artikel</h2>
+            <p>Edit artikel</p>
+        
+        <?php
+
+            include "koneksi.php";
+
+            // mengambil data id
+            $id = $_GET['id'];
+
+            // ambil data dari table posts
+            $data = mysqli_query($connect, "SELECT * FROM posts WHERE id_post=$id");
+
+            // melakukan perulangan pada data ARRAY untuk ditampilkan
+            while($d = mysqli_fetch_array($data)){
+        ?>
+
+            <div class="form-tambah-artikel">
+                <form action="tambah-artikel-post.php" method="POST">
+                    <div class="field-artikel">
+                        <label>Judul</label> <br>
+                        <input type="hidden" name="id" value="<?php echo $d['id_post'] ?>">
+                        <input type="text" name="judul" value="<?php echo $d['title_post'] ?>">
+                    </div>
+                    <div class="field-artikel">
+                        <label>Artikel</label> <br>
+                        <textarea name="artikel"cols="30" rows="10">
+                            <?php echo $d['article'] ?>
+                        </textarea>
+                    </div>
+                    <div class="field-artikel">
+                        <input type="submit" value="SIMPAN">
+                    </div>
+                </form>
+            </div>
+        <?php
+            }
+        ?>
+
+        </div> <!-- tutup content -->
+
+    </div> <!-- tutup container -->
+    
+</body>
+</html>
